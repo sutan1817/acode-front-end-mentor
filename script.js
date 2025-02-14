296cdbe92cbb0fb03fe73ae889939f1691e198e8
sutan1817 <sutan.sinaga.18@gmail.com>
2025-02-14 10:03:33 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-person{
+class person{
   constructor(nama,age,country){
-    this.name=name;
-    this.age=age;
-    this.country=country;
+    this.name = name;
+    this.age = age;
+    this.country = country;
   }
+  default() {
+  console.log(`name: ${this.name} `);
+  console.log(`age: ${this.age}`);
+  console.log(`country:${this.country}`);
+ }
 }
-default(){
-  console.log('name: ${this.name}');
-  console.log('age: ${this.age}');
-  console.log('country: ${this.country}');
-}
-person1=
+person1 =new person('fransisco',23,'italia');
+person2 =new person('hera',21,'greeks');
+person1.default();
