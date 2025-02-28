b1404b1f1aa3f18ec2c1426f6c256d191d50772d
sutan1817 <sutan.sinaga.18@gmail.com>
2025-02-28 12:17:40 +0700

    update password generator/script.js

---
+++ password generator/script.js
@@ -0,0 +1,18 @@
+const form=document.getElementbyId('form')
+const uploud=document.getElementbyId('uploud-div')
+const input=document.getElementbyclass('input-form')
+const generatebutton=document.getElementbyId('generate-button')
+const generatetickets=doccument.getElementbyId('generate-tickets')
+const button1=doccument.getElementbyId('button1')
+const button2=doccument.getElementbyId('button2')
+  
+
+
+const formdata = {
+  name:'',
+  github:'',
+  email:'',
+  image:''
+  }
+input-form.addEventlistener('dragover'(e)=>{
+  (e.preventdefault})
